4236d874134940dc59191ce73ab163268aad8beb
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-07-12 08:07:13 +0300

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vesen/adminlte',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '347a53ad9a828cb6b47fb821f0602cfe790d2fbe',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vesen/adminlte' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '347a53ad9a828cb6b47fb821f0602cfe790d2fbe',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
